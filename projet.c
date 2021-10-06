a95c02c79beab7e8e99e1f8f7fec6611d197ffd3
integrateur-web <59866337+integrateur-web@users.noreply.github.com>
2021-10-06 18:42:29 +0200

    Fix bad repo

---
+++ projet.c
@@ -0,0 +1,19 @@
+/******
+
+Niveau 1 : 
+Chaque catégorie (filmCategorieEnfant, filmCategorieAdos, filmCategorieAdulte) de film correspond à un tableau de données (trois films par catégorie). 
+
+- Si elle a moins de 11 ans, elle peut aller voir Films Categorie Enfant
+- Si elle a entre 11 et 17, elle peut aller voir Films Categorie Ados
+- Si elle a plus de 18 ans, elle peut aller voir Films Categorie Adulte
+
+Niveau 2 : 
+L'utilisateur peut dire si oui ou non il souhaite aller voir le film proposé. 
+Par exemple : 
+Film catégorie adulte contient "The Shining", "La Nonne" et "Conjuring". On propose à une personne ayant 20 ans d'aller voir The Shining, si elle dit oui, on s'arrête dans les propositions, 
+sinon, on lui propose La Nonne. Idem pour le dernier élément du tableau. 
+Si elle ne veut voir aucun de ces trois films. 
+
+Niveau 3 (optionnel) : 
+Si aucun des films de la catégorie adulte ne lui convient, on lui propose de choisir une autre catégorie (une catégorie enfant ou ados), en lui proposant à nouveau les choix respectifs de ces catégories.*************/
+

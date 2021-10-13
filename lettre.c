888a165fbb785b18d51b608bf217bf413ea94957
dylanbourdais <90897072+dylanbourdais@users.noreply.github.com>
2021-10-13 13:52:19 +0200

    Add files via upload

---
+++ lettre.c
@@ -0,0 +1,42 @@
+#include <stdio.h> // librairie stdio
+#include <cs50.h> //librairie cs50
+
+
+int main(void)
+{
+
+    int gramme = get_int("Combien de gramme votre lettre : "); //variable gramme , ecrire combien gramme votre lettre , et demande utulisateur
+
+    if (gramme <= 20) // si gramme est inferieur 20 gramme alors
+    {
+       printf(" Pour %i g vous avez besoin 1 timbres \n", gramme); // affiche pour 20 gramme vous avez besoin 1 timbres
+    }
+
+    else if (gramme >= 21  &&  gramme <= 100) // sinon si  gramme est superieur egal 21 et gramme inferieur egal 100 alors
+    {
+       printf(" Pour %i g vous avez besoin 2 timbres \n", gramme); // affiche pour 21 jusque a 100 gramme vous avez besoin 2 timbres
+    }
+
+    else if (gramme >= 101 &&  gramme <= 250) // sinon si  gramme est superieur egal 101 et gramme inferieur egal 250 alors
+    {
+       printf(" Pour %i g vous avez besoin 4 timbres \n", gramme); // affiche pour 101 jusque a 250 gramme vous avez besoin 4 timbres
+    }
+
+    else if (gramme >= 251 && gramme <= 500) // sinon si  gramme est superieur egal 251 et gramme inferieur egal 500 alors
+    {
+       printf("Pour %i g vous avez besoin 6 timbres \n", gramme); // affiche pour 251 jusque a 500 gramme vous avez besoin 6 timbres
+    }
+
+    else if (gramme >= 501 && gramme <= 3000) // sinon si  gramme est superieur egal 501 et gramme inferieur egal 3000 alors
+    {
+       printf("Pour %i g vous avez besoin 8 timbres \n", gramme); // affiche pour 501 jusque a 3000 gramme vous avez besoin 8 timbres
+    }
+
+    else // sinon
+    {
+       printf("Allez voir la facteur ou la factrice \n"); // affiche allez voir la facteur ou la factrice
+    }
+
+}
+
+
---
+++ lettre.py
@@ -0,0 +1,26 @@
+gramme = int(input("Combien de gramme votre lettre : ")) # variable gramme , ecrire combien gramme votre lettre , et demande utulisateur
+
+
+if gramme <=20: # si gramme est inferieur 20 gramme alors
+
+    print("Pour",gramme,"g vous avez besoin 1 timbres ") # affiche pour 20 gramme vous avez besoin 1 timbres
+
+elif gramme >=21 & gramme <=100: #sinon si  gramme est superieur egal 21 et gramme inferieur egal 100 alors
+
+    print("Pour",gramme,"g vous avez besoin 2 timbres ") #affiche pour 21 jusque a 100 gramme vous avez besoin 2 timbres
+
+elif gramme >=101 & gramme <=250: #sinon si  gramme est superieur egal 101 et gramme inferieur egal 250 alors
+
+    print("Pour",gramme,"g vous avez besoin 4 timbres ") #  affiche pour 101 jusque a 250 gramme vous avez besoin 4 timbres
+
+elif gramme >=251 & gramme <=500: # sinon si  gramme est superieur egal 251 et gramme inferieur egal 500 alors
+
+    print("Pour",gramme,"g vous avez besoin 6 timbres ") # affiche pour 251 jusque a 500 gramme vous avez besoin 6 timbres
+
+elif gramme >=501 & gramme <=3000: #  sinon si  gramme est superieur egal 501 et gramme inferieur egal 3000 alors
+
+    print("Pour",gramme,"g vous avez besoin 8 timbres ") # affiche pour 501 jusque a 3000 gramme vous avez besoin 8 timbres
+
+else: # sinon
+
+    print("Allez voir la facteur ou la factrice ") # affiche allez voir la facteur ou la factrice

50e695c906613631da758643c59c228a52b1b3b2
dylanbourdais <90897072+dylanbourdais@users.noreply.github.com>
2021-10-13 15:39:28 +0200

    Add files via upload

--- lettre.py
+++ lettre.py
@@ -6,19 +6,19 @@ if gramme <=20: # si gramme est inferieur 20 gramme alors
 
     print("Pour",gramme,"g vous avez besoin 1 timbres ") # affiche pour 20 gramme vous avez besoin 1 timbres
 
-elif gramme >=21 & gramme <=100: #sinon si  gramme est superieur egal 21 et gramme inferieur egal 100 alors
+elif gramme >=21 and gramme <=100: #sinon si  gramme est superieur egal 21 et gramme inferieur egal 100 alors
 
     print("Pour",gramme,"g vous avez besoin 2 timbres ") #affiche pour 21 jusque a 100 gramme vous avez besoin 2 timbres
 
-elif gramme >=101 & gramme <=250: #sinon si  gramme est superieur egal 101 et gramme inferieur egal 250 alors
+elif gramme >=101 and gramme <=250: #sinon si  gramme est superieur egal 101 et gramme inferieur egal 250 alors
 
     print("Pour",gramme,"g vous avez besoin 4 timbres ") #  affiche pour 101 jusque a 250 gramme vous avez besoin 4 timbres
 
-elif gramme >=251 & gramme <=500: # sinon si  gramme est superieur egal 251 et gramme inferieur egal 500 alors
+elif gramme >=251 and gramme <=500: # sinon si  gramme est superieur egal 251 et gramme inferieur egal 500 alors
 
     print("Pour",gramme,"g vous avez besoin 6 timbres ") # affiche pour 251 jusque a 500 gramme vous avez besoin 6 timbres
 
-elif gramme >=501 & gramme <=3000: #  sinon si  gramme est superieur egal 501 et gramme inferieur egal 3000 alors
+elif gramme >=501 and gramme <=3000: #  sinon si  gramme est superieur egal 501 et gramme inferieur egal 3000 alors
 
     print("Pour",gramme,"g vous avez besoin 8 timbres ") # affiche pour 501 jusque a 3000 gramme vous avez besoin 8 timbres
 
